367a81bbe041209147b1f214156502da32852a1b
Munavar PM <munavar@smarthatch.com>
2025-07-17 11:27:11 +0530

    Initial Commit

---
+++ Appadore/AppadoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppadoreApp.swift
+//  Appadore
+//
+//  Created by Munavar on 17/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppadoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
